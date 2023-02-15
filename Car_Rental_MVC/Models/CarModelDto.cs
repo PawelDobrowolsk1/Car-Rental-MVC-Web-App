301d7781440d80e721c195c39c976576054df823
PawelDobrowolsk1 <pdobrowolski001@gmail.com>
2023-02-16 00:42:45 +0100

    Adding HTML helper(AntiForgeryToken) to pageViews-Adding NuGet to prevent XSS(Cross-Site Scripting)

--- Car_Rental_MVC/Models/CarModelDto.cs
+++ Car_Rental_MVC/Models/CarModelDto.cs
@@ -7,26 +7,27 @@ namespace Car_Rental_MVC.Models
     public class CarModelDto
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Category is required")]
         public string Category { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Make is required")]
         public string Make { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Model is required")]
         public string Model { get; set; }
         [Required(ErrorMessage = "Engine capacity is required")]
-        public decimal Engine { get; set; }
+        public decimal? Engine { get; set; }
         [Required(ErrorMessage = "Horsepower is required")]
-        public int Horsepower { get; set; }
+        public int? Horsepower { get; set; }
         [Required(ErrorMessage = "The year of the car is required")]
-        public int Year { get; set; }
+        [Range(1886,2023)]
+        public int? Year { get; set; }
         [Required(ErrorMessage = "The number of seats is required")]
-        public int Seats { get; set; }
+        public int? Seats { get; set; }
         [Required(ErrorMessage = "The number of doors is required")]
-        public int Doors { get; set; }
+        public int? Doors { get; set; }
         [Required(ErrorMessage = "Car fuel is required")]
         public string? Fuel { get; set; }
         [Required(ErrorMessage = "The transmisson is required")]
-        public string Transmisson { get; set; }
+        public string? Transmisson { get; set; }
         [DisplayName("Description:")]
         public string? Description { get; set; }
         public bool Available { get; set; }
